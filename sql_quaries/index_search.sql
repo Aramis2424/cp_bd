ea31be97c9f14848a59a58eea23df57d80ac5f1a
Aramis2424 <srd2424@yandex.ru>
2024-06-02 00:42:41 +0300

    upd sql script

--- sql_quaries/index_search.sql
+++ sql_quaries/index_search.sql
@@ -4,5 +4,11 @@ select * from bookings;
 CREATE INDEX is_active_index ON bookings (is_active);
 drop index is_active_index;
 
+CREATE INDEX is_active_index ON bookings (is_active) WHERE is_active;
+drop index is_active_index;
+
 EXPLAIN ANALYZE SELECT * FROM bookings WHERE is_active = true;
 SELECT * FROM bookings WHERE is_active = true;
+
+EXPLAIN ANALYZE SELECT * FROM bookings WHERE bookingid < 100;
+SELECT * FROM bookings WHERE bookingid < 100;
